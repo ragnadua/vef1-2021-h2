403ec87e59a666f671a0b478382211f52ac53dad
ragnadua <80420790+ragnadua@users.noreply.github.com>
2021-11-26 23:19:54 +0000

    lett hata þetta rn

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Sækja storage gögn og láta þau virka með þessu
 
-import { fetchData, isCompleted } from "./lib/storage.js"; 
+
+import { fetchData, fetchAndRenderCategory } from "./lib/storage.js"; 
 
 
 
@@ -9,4 +10,20 @@ const gogn = await fetchData();
 const localgogn = JSON.parse(window.localStorage.getItem('localStorageKey'));
 console.log(localgogn);
 //console.log(gogn);
-isCompleted();
+
+/*const plis = fetchAndRenderCategory('all', null,null);
+console.log(plis);*/ 
+
+async function route(){
+
+    const container = document.querySelector(".pin");
+    console.log(await fetchAndRenderCategory('all',null, container), "lovely");
+   
+}
+
+async function test(){
+    let test = await fetchData();
+    let tes1 = route();
+
+}
+test();
